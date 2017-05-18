6cf4249b33f2dadf4cb2e94a9911e9a260c6ccd0
zano <adrian@xenotype.com>
2017-05-18 10:42:29 +0200

    Move LimitedList to Lib

--- Lib/LimitedList.cs
+++ Lib/LimitedList.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Donjon.Entities;
 
-namespace Donjon {
-    class LimitedList<T> : IEnumerable<T> {
-        private List<T> items;
+namespace Lib {
+    public class LimitedList<T> : IEnumerable<T> {
+        private readonly List<T> items;
 
         public int Capacity { get; }
         public int Count => items.Count;
