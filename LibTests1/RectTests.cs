3c49bede436bca0acaaa29f9d27e7e493cbbfa67
zano <adrian@xenotype.com>
2017-06-05 01:06:04 +0200

    Add tests for new code

---
+++ LibTests1/RectTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Tests
+{
+    [TestClass()]
+    public class RectTests
+    {
+        [TestMethod()]
+        public void NormalizedTest()
+        {
+            Rect rectangle = new Rect { X = 2, Y = 3, Width = 4, Height = 5 };
+            Rect rectangle2 = new Rect { X = 2, Y = 3, Width = 4, Height = 5 };
+            Rect normalized = rectangle.Normalized();
+            Assert.AreEqual(rectangle.X, rectangle2.X);
+            Assert.AreEqual(rectangle.X, normalized.X);
+            Assert.AreEqual(rectangle2.X, normalized.X);
+        }
+        [TestMethod()]
+        public void NormalizedWidthIsNegativeTest()
+        {
+            Rect rectangle = new Rect { X = 2, Y = 3, Width = -4, Height = 5 };
+            Rect expected = new Rect { X = -2, Y = 3, Width = 4, Height = 5 };
+            Rect normalized = rectangle.Normalized();
+            Assert.AreEqual(normalized.X, expected.X);
+           
+        }
+    }
+}
---
+++ MazeConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib;
+
+namespace MazeConsole {
+    class Program {
+        static void Main(string[] args) {
+            var symbols = new Dictionary<CellType, string> {
+                { CellType.Door, "x" },
+                { CellType.Wall, "#" },
+                { CellType.Empty, "." },
+            };
+
+            var width = 100;
+            var height = 30;
+            var ui = new Ui(width+4, height+12);
+            var maze = new Maze(width, height);
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    ui.Write(symbols[maze.Map[x, y]]);
+                }
+                ui.WriteLine();
+            }
+            ui.AskForKey("");
+        }
+    }
+}
